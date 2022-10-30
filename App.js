9aedb055af79cac33ada9445875f6954f7e7d310
OscarArista <20181169@uthh.edu.mx>
2022-10-30 14:44:32 -0600

    se agrega el proyecto

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import NavigationContenedor from './App/NavigationContainer/NavigationContenedor';
+
+//OPCION 1: CREAR COMPONENTE FUNCION
+const App = () =>{
+  return (
+    <NavigationContenedor/> // Llamar al Navegador de raiz
+  )
+};
+
+export default App;
+
+
+// OPCION 2: CREAR COMPONENTE FUNCION
+// function App(){
+//   return (
+//     <NavigationContenedor/> // Llamar al Navegador de raiz
+//   )
+// };
+
+// export default App;
